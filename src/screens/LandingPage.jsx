f58c84f6d209eef652d8f39f4e508d0a7fa9e72e
Diego A. Romero Ulloa <47290890+DiEGO-ROM@users.noreply.github.com>
2024-11-26 18:45:27 -0600

    Bug Fixes

--- src/screens/LandingPage.jsx
+++ src/screens/LandingPage.jsx
@@ -1,27 +1,27 @@
-import React from "react";
 
 const LandingPage = () => {
-  return (
-    <div style={styles.container}>
-      <h1 style={styles.title}>Welcome to the Landing Page!</h1>
-    </div>
-  );
-};
-
-const styles = {
-  container: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    height: "100vh", // Ocupa toda la altura de la pantalla
-    width: "100vw", // Ocupa todo el ancho de la pantalla
-    backgroundColor: "rgb(38, 51, 93)",
-    color: "#ffffff",
-    fontFamily: "Arial, sans-serif",
-  },
-  title: {
-    fontSize: "2.5rem",
-  },
-};
-
-export default LandingPage;
+    return (
+      <div style={styles.container}>
+        <h1 style={styles.title}>Welcome to the Landing Page!</h1>
+      </div>
+    );
+  };
+  
+  const styles = {
+    container: {
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100vh", // Ocupa toda la altura de la pantalla
+      width: "100vw", // Ocupa todo el ancho de la pantalla
+      backgroundColor: "rgb(38, 51, 93)",
+      color: "#ffffff",
+      fontFamily: "Arial, sans-serif",
+    },
+    title: {
+      fontSize: "2.5rem",
+    },
+  };
+  
+  export default LandingPage;
+  
